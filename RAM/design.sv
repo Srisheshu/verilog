21a4f2d9686a1930f9dac6ed46898e6255c91dbe
Srisheshu <sriseshupolisetti7@gmail.com>
2026-07-27 16:06:59 +0530

    Add files via upload

---
+++ RAM/design.sv
@@ -0,0 +1,19 @@
+module single_port_ram(
+  input [7:0] data,
+  input [5:0] addr, 
+  input we, 
+  input clk, 
+  output [7:0] q
+);
+  
+  reg [7:0] ram [63:0];
+ 
+  always @ (posedge clk)
+    begin
+      if(we)
+        ram[addr] <= data;
+    end
+ 
+  assign q = ram[addr_reg];
+  
+endmodule
