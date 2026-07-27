47ecb92d18063096e18407669f4b08fdc3d1ca37
Srisheshu <sriseshupolisetti7@gmail.com>
2026-07-27 13:32:42 +0530

    Add files via upload

---
+++ step 4 counter/design.sv
@@ -0,0 +1,13 @@
+// Code your design here
+module counter(q,clk,reset);
+  input clk,reset;
+  output reg [3:0]q;
+  
+  always @(posedge clk )
+    begin
+      if (reset)
+        q<=4'b0000;
+      else 
+        q<=q+3'b100;
+    end 
+endmodule
---
+++ step 4 counter/testbench.sv
@@ -0,0 +1,29 @@
+// Code your testbench here
+// or browse Examples
+module tb;
+  reg clk,reset;
+  wire [3:0]a;
+  
+  counter COUNTER1(.q(a),.clk(clk),.reset(reset));
+  //counter COUNTER2(.q(b),.clk(clk),.clear(reset));
+  
+  initial begin
+    
+    $dumpfile("tff_wave.vcd");   
+    $dumpvars(1, tb); 
+    clk=0;
+    reset=1;
+    #20 reset =0;
+    #60 reset=1;
+    #90 reset=0;
+    
+   // #10 d=1;
+    #300 $finish;
+  end
+    always #5 clk=~clk;
+     //always #10 d=~d;
+  
+ 
+  
+  
+endmodule
