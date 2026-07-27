bd9f7912827452fd84fa3919421056a14785765c
Srisheshu <sriseshupolisetti7@gmail.com>
2026-07-27 13:40:30 +0530

    Add files via upload

---
+++ MOD 16 counter/testbench.sv
@@ -0,0 +1,23 @@
+// Code your testbench here
+// or browse Examples
+module tb;
+  reg clk,reset;
+  wire [3:0]a;
+  
+  counter COUNTER1(.q(a),.clk(clk),.reset(reset));
+  
+  initial begin
+    
+    $dumpfile("tff_wave.vcd");   
+    $dumpvars(1, tb); 
+    clk=0;
+    reset=1;
+    #20 reset =0;
+    #250 reset=1;
+    #20 reset=0;
+   
+    #300 $finish;
+  end
+    always #5 clk=~clk;
+  
+endmodule
